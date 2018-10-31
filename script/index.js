129aa3ab04ad007d759a644a3cf5c23e4b906f6c
YouNesta <boulkaddid.younes@gmail.com>
2018-10-31 16:58:56 +0100

    Remove useless import and call files with git link

--- script/index.js
+++ script/index.js
@@ -1,21 +1,11 @@
-import autocomplete from "autocompleter";
-
-var countries = [
-  { label: "United Kingdom", value: "UK" },
-  { label: "United States", value: "US" }
-];
-
 autocomplete({
   input: document.getElementById("autocomplete"),
   fetch: function(text, update) {
     text = text.toLowerCase();
-    // you can also use AJAX requests instead of preloaded data
-    var suggestions = countries.filter(n =>
-      n.label.toLowerCase().startsWith(text)
-    );
-    update(suggestions);
+    // Fetch call and set it in input with update callback
+    update();
   },
   onSelect: function(item) {
-    alert(item.value); // will display 'US' or 'UK'
+    alert(item.value);
   }
 });
